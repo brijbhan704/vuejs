86e123f1cafe7593bb1eb4f59e9c56a90e5a28f9
brijbhan704 <55311169+brijbhan704@users.noreply.github.com>
2020-04-28 16:30:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+				var myabj = new Vue({
+				el:"#our-app",  //el means element
+				data:{        // data is type of object
+
+					message:"this is our first code thank you",
+					fname:"Brijbhan",
+					lname:"Tiwari",
+				}
+
+			});
